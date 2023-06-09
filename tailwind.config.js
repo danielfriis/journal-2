88f1d54a8530b5be3e54bc76fedc164f99f0b5a1
danielfriis <danielfriis@users.noreply.github.com>
2023-06-09 13:25:01 +0000

    deploy: a39db68b025b8dda063ed02b5472e04e663ad328

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,32 @@ module.exports = {
     './*.md',
     './*.html',
   ],
+  safelist: [
+    'col-span-1',
+    'col-span-2',
+    'col-span-3',
+    'col-span-4',
+    'col-span-5',
+    'col-span-6',
+    'col-span-7',
+    'col-span-8',
+    'col-span-9',
+    'col-span-10',
+    'lg:col-span-1',
+    'lg:col-span-2',
+    'lg:col-span-3',
+    'lg:col-span-4',
+    'lg:col-span-5',
+    'lg:col-span-6',
+    'lg:col-span-7',
+    'lg:col-span-8',
+    'lg:col-span-9',
+    'lg:col-span-10'
+  ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen-Sans', 'Ubuntu', 'Cantarell', 'Helvetica Neue', 'sans-serif']
+        // sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen-Sans', 'Ubuntu', 'Cantarell', 'Helvetica Neue', 'sans-serif']
       }
     }
   },
